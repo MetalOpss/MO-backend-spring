d2c1d181fd440ce537e46aa2d226ec82781b615a
neB <benjamin.sullca1103@gmail.com>
2025-09-19 21:49:20 -0500

    fix: change to package name repository to repositories

--- src/main/java/com/backend_spring/auth/repositories/UsuarioRepository.java
+++ src/main/java/com/backend_spring/auth/repositories/UsuarioRepository.java
@@ -1,6 +1,5 @@
-package com.backend_spring.auth.repository;
+package com.backend_spring.auth.repositories;
 
-import com.backend_spring.auth.models.Rol;
 import com.backend_spring.auth.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
