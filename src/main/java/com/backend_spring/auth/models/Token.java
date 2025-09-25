d82bc37cfa0205b68fe16c3c1ec0b288eaac9db5
neB <benjamin.sullca1103@gmail.com>
2025-09-25 10:32:56 -0500

    feat: model Token

---
+++ src/main/java/com/backend_spring/auth/models/Token.java
@@ -0,0 +1,42 @@
+package com.backend_spring.auth.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+
+    public enum TokenType {
+        BEARER
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType =  TokenType.BEARER;
+
+    private boolean revoked;
+    private boolean expired;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+}
