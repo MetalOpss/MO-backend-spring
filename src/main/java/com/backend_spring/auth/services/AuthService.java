65caeb91dac9ae784dc900bb953e6ee0f4d0dd6c
neB <benjamin.sullca1103@gmail.com>
2025-09-25 10:49:18 -0500

    feat: method PasswordEncoder in AppConfig for hashing in passwords

---
+++ src/main/java/com/backend_spring/auth/services/AuthService.java
@@ -0,0 +1,11 @@
+package com.backend_spring.auth.services;
+
+import com.backend_spring.auth.dto.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+}
