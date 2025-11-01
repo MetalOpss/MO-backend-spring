555bff35f9677c3e0976d4f2cb0618213bce7e8b
neB <benjamin.sullca1103@gmail.com>
2025-11-01 16:45:07 -0500

    feat: delete models Maquina & Servicio

--- src/main/java/com/backend_spring/auth/dto/ServicioUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.backend_spring.auth.dto;
-
-import jakarta.validation.constraints.Size;
-
-public record ServicioUpdateRequest(
-        @Size(max = 100) String nombre,
-        @Size(max = 100) String descripcion,
-        Long idMaquina
-) {}
--- src/main/java/com/backend_spring/auth/repository/MaquinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend_spring.auth.repository;
-
-import com.backend_spring.auth.models.Maquina;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MaquinaRepository extends JpaRepository<Maquina, Long> {
-}
